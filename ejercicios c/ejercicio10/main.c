188740a25ca689fa830a3b3cc993fb5a0f6c14b6
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-08-21 20:57:56 -0300

    Update main.c

--- ejercicios c/ejercicio10/main.c
+++ ejercicios c/ejercicio10/main.c
@@ -1,5 +1,5 @@
 //Escribir
-//en C un programa que muestre los números impares entre
+//en C un programa que muestre los nÃºmeros impares entre
 //0 y 100 y que imprima cuantos impares hay
 
 #include <stdio.h>
@@ -13,20 +13,16 @@ int contador=0;
 int contadorImpar=0;
 
 
-while(contador<=100)
+while(contador<100)
 {
-
-
-printf("%d\n",contador);
 
 contador++;
 
 if(contador%2==1)
 {
-contadorImpar++;
+printf("Los numeros impares son: %d\n" ,contador);
 }
 }
-printf("La cantidad de numeros impares es de: %d" ,contadorImpar);
 system("pause");
 return 0;
 }
