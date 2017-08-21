5f9e5e6ee9db8ec9cb195f6bcb6335c310b24196
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-08-21 20:18:35 -0300

    Update main.c

--- ejercicios c/ejercicio6/main.c
+++ ejercicios c/ejercicio6/main.c
@@ -1,4 +1,4 @@
-//6. De 10 números ingresadas indicar cuantos son mayores a cero y cuantos son menores a cero
+//6. De 10 nÃºmeros ingresadas indicar cuantos son mayores a cero y cuantos son menores a cero
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,26 +8,31 @@ int main()
 int contador=0;
 int contadorPositivos=0;
 int contadorNegativos=0;
+int contadorCeros=0;
 int numero;
 
 while(contador<10)
    {
     printf("Ingrese 10 numeros: ");
+    fflush(stdin);
     scanf("%d",&numero);
 
     contador++;
     }
 
-if(numero>=0)
+if(numero>0)
      {
      contadorPositivos++;
      }
-
+else if(numero==0)
+   {
+    contadorCeros++;
+   }
 else
     {
     contadorNegativos++;
     }
 
-printf("La cantidad de numeros ingresados mayores a 0 son: %d La catidad de numeros ingresados menores a 0 son: %d" ,contadorPositivos,contadorNegativos);
+printf("La cantidad de numeros ingresados mayores a 0 son: %d\n La cantidad de ceros ingresada es de: %d\n La catidad de numeros ingresados menores a 0 son: %d" ,contadorPositivos,contadorCeros,contadorNegativos);
     return 0;
 }
